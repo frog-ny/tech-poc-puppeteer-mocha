1c755691531dc8612349a74816e1a14eae4d3ac1
Jeff Ong <jeffrey.ong@frogdesign.com>
2018-11-08 13:19:01 -0500

    testing attribute value of custom web component

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,10 +1,9 @@
 const { expect } = require('chai');
 
-describe('sample test', function() {
+describe('button test', function() {
   let page;
 
   before (async function() {
-    this.timeout(5000);
     page = await browser.newPage();
     await page.goto('http://localhost:3000/test'); // needs to map to the http-server and exposed port
   });
@@ -13,13 +12,42 @@ describe('sample test', function() {
     await page.close();
   });
 
-  it('should have one button', async function() {
-    this.timeout(5000);
-    const BUTTON_SELECTOR = '.counter-button';
-    const title = await page.title();
+  it('should have one custom element', async function() {
+    const COUNTER_SELECTOR = 'exa-counter';
 
-    const button = await page.waitFor(BUTTON_SELECTOR);
+    await page.waitFor(COUNTER_SELECTOR);
+
+    // const counter = await page.$$eval(COUNTER_SELECTOR, e => e.getAttribute('value'));
+    const counter = await page.$$(COUNTER_SELECTOR);
+
+    expect(counter).to.have.lengthOf(1);
+  });
+  
+  it('should have one button', async function() {
+    const BUTTON_SELECTOR = '.counter-button';
+
+    await page.waitFor(BUTTON_SELECTOR);
 
     expect(await page.$$(BUTTON_SELECTOR)).to.have.lengthOf(1);
+  });
+
+  it('should display the counter', async function() {
+    const COUNT_SELECTOR = '.counter-count';
+
+    await page.waitFor(COUNT_SELECTOR);
+
+    expect(await page.$$(COUNT_SELECTOR)).to.have.lengthOf(1);
+  });
+
+  it('count should be zero to begin', async function() {
+    const COUNTER_SELECTOR = 'exa-counter';
+
+    await page.waitFor(COUNTER_SELECTOR);
+
+    const counter = await page.$$eval(COUNTER_SELECTOR, (e) => {
+      return e[0].getAttribute('value');
+    });
+
+    expect(counter).to.eql(0);
   });
 });
